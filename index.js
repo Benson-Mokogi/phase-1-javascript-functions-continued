b3d71c9e01163d5ea7dfc3b5119fb0570deb0784
Benson Mokogi <b.mokogi@gmail.com>
2023-03-15 02:13:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return(`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+
+function mondayWork(whatTodo = "go to the office"){
+return(`This Monday, I will ${whatTodo}.`);
+};
+mondayWork();
+
+function wrapAdjective(mark = "*") {
+    return function(adj) {
+      return `You are ${mark}${adj}${mark}!`;
+    };
+  }
+  
